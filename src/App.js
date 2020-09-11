334d59a51590e20e359defbc0ef358078f261d46
Arun Goel <agoel2@atlassian.com>
2020-09-11 17:07:59 +0530

    Full api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      prson: null,
+    };
+  }
+
+  async componentDidMount() {
+    const url = "https://api.randomuser.me/";
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data.results[0].gender);
+    this.setState({ person: data.results[0], loading: false });
+  }
+
+  render() {
+    return (
+      <div>
+        {!this.state.person ? (
+          <div> loading... </div>
+        ) : (
+          <div>
+            {" "}
+            {this.state.person.name.title} {this.state.person.name.first}{" "}
+            {this.state.person.name.last}{" "}
+            <img src={this.state.person.picture.large} />
+          </div>
+        )}
+      </div>
+    );
+  }
 }
-
-export default App;
